5645a439d89b63b4949864f798e2eb2ce63ed221
athompson-git <athompson@northwestern.edu>
2025-10-03 10:09:08 -0700

    fixing comment on units MeV -> GeV

--- constants.py
+++ constants.py
@@ -69,7 +69,7 @@ OMEGA_DM = OMEGA_MATTER - OMEGA_BARYON
 
 
 # conversion between units
-GEV_PER_G = 5.6095887e23  # MeV/g
+GEV_PER_G = 5.6095887e23  # GeV/g
 MEV_PER_KG = 5.6095887e29  # MeV/kg
 MEV_PER_G = 5.6095887e26  # MeV/g
 MEV_PER_HZ = 6.58e-22  # 1 s^-1 = 6.58e-22 MeV
